161fcc7db3e810b218030fca132323a07713bab5
Majeed <majeedaskari96@gmail.com>
2019-01-24 00:51:47 +0330

    minor fix

--- migrations/user.py
+++ migrations/user.py
@@ -16,10 +16,10 @@ mycursor.execute(
     "password VARCHAR(150),salt VARCHAR(150),email VARCHAR(32),birth_year TINYINT, postal_code VARCHAR(32),"
     "address TEXT, weight TINYINT, gender TINYINT, height TINYINT, state TINYINT, role char(50)) ENGINE = InnoDB")
 
-db.commit()
+mydb.commit()
 
 mycursor.execute(
     "CREATE TABLE IF NOT EXISTS `hospital`.`api_keys` ( `api_key_id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(50) NOT NULL\
      , `api_key` TEXT NOT NULL , `exp_date` DATETIME NULL , PRIMARY KEY (`api_key_id`)) ENGINE = InnoDB;")
 
-db.commit()
+mydb.commit()
