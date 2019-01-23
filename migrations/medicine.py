035765ef0ba992b4ddf2d9f040742154d92020c8
nimamoradi <ni_moradi@yahoo.com>
2019-01-23 20:21:59 +0330

    medicine

---
+++ migrations/medicine.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from config import database
+
+mydb = mysql.connector.connect(
+    host=database['host'],
+    user=database['user'],
+    passwd=database['passwd'],
+    database=database['db_name']
+)
+
+cursor = mydb.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS medicine (id  int(10) PRIMARY KEY AUTO_INCREMENT,"
+               "name char(50) ,price int(12),exp_date DATETIME)")
